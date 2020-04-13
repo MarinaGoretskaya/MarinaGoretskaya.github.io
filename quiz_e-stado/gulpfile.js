9832eabdc2fb80e2e632b9b3c857bc799e834096
MarinaGoretskaya <34473616+MarinaGoretskaya@users.noreply.github.com>
2020-04-13 09:40:13 +0300

    Add files via upload

---
+++ quiz_e-stado/gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const jquery = require('jquery');
+const watch = require('gulp-watch');
+
+const cssFiles = [
+	'./node_modules/jquery/dist/css/bootstrap.min.css',
+	'./scss/**/*.scss'
+]
+
+const jsFiles = [
+	'./node_modules/bootstrap/dist/js/bootstrap.min.js',
+	'./node_modules/jquery/dist/css/bootstrap.min.css',
+	'./js/main.js'
+]
+
+function scripts() {
+	return gulp.src(jsFiles)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('./js'))
+}
+
+gulp.task('sass-compile', function () {
+	// return gulp.src('./scss/**/*.scss')
+	return gulp.src(cssFiles)
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('./css/'))
+})
+
+gulp.task('watch', function () {
+	gulp.watch('./scss/**/*.scss', gulp.series('sass-compile'))
+	gulp.watch('./js/**/*.js', scripts);
+})
+
+gulp.task('scripts', scripts);
+
